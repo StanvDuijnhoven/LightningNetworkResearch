04639da923afd659619b15aabf11e96ec1f6a1fa
StanvDuijnhoven <s.p.j.vanduijnhoven@students.uu.nl>
2020-04-22 14:16:28 +0200

    Cleaned variables + Rstudio cleaning script

---
+++ Cleaned variables/cleaning script.R
@@ -0,0 +1,46 @@
+#import required libraries
+library(stringr)
+library(readxl)
+library("xlsx")
+
+#set working directory
+setwd("C:/Users/Stan/Downloads/OZP/results/Extracted variables Excel/")
+
+#import sheet
+paymentsPC_noTOR <- read_excel("paymentsPC_noTOR.xlsx")
+
+#extract numeric values of each variable and save it
+paymentsPC_noTOR$PaymentIndex <- as.numeric(str_extract_all(paymentsPC_noTOR$PaymentIndex, "[0-9]+"))
+paymentsPC_noTOR$AttemptTime <- as.numeric(str_extract_all(paymentsPC_noTOR$AttemptTime, "[0-9]+"))
+paymentsPC_noTOR$ResolveTime <- as.numeric(str_extract_all(paymentsPC_noTOR$ResolveTime, "[0-9]+"))
+paymentsPC_noTOR$TotalFeesMilisatoshis <- as.numeric(str_extract_all(paymentsPC_noTOR$TotalFeesMilisatoshis, "[0-9]+"))
+paymentsPC_noTOR$TransactionValue <- as.numeric(str_extract_all(paymentsPC_noTOR$TransactionValue, "[0-9]+"))
+
+#save dataframe as .xlsx
+write.xlsx(paymentsPC_noTOR, "paymentsPC_noTOR_cleaned.xlsx")
+
+#import sheet
+paymentsPi_noTOR <- read_excel("paymentsPi_noTOR.xlsx")
+
+#extract numeric values of each variable and save it
+paymentsPi_noTOR$PaymentIndex <- as.numeric(str_extract_all(paymentsPi_noTOR$PaymentIndex, "[0-9]+"))
+paymentsPi_noTOR$AttemptTime <- as.numeric(str_extract_all(paymentsPi_noTOR$AttemptTime, "[0-9]+"))
+paymentsPi_noTOR$ResolveTime <- as.numeric(str_extract_all(paymentsPi_noTOR$ResolveTime, "[0-9]+"))
+paymentsPi_noTOR$TotalFeesMilisatoshis <- as.numeric(str_extract_all(paymentsPi_noTOR$TotalFeesMilisatoshis, "[0-9]+"))
+paymentsPi_noTOR$TransactionValue <- as.numeric(str_extract_all(paymentsPi_noTOR$TransactionValue, "[0-9]+"))
+
+#save dataframe as .xlsx
+write.xlsx(paymentsPi_noTOR, "paymentsPi_noTOR_cleaned.xlsx")
+
+#import sheet
+paymentsPC_TOR <- read_excel("paymentsPC_TOR.xlsx")
+
+#extract numeric values of each variable and save it
+paymentsPC_TOR$PaymentIndex <- as.numeric(str_extract_all(paymentsPC_TOR$PaymentIndex, "[0-9]+"))
+paymentsPC_TOR$AttemptTime <- as.numeric(str_extract_all(paymentsPC_TOR$AttemptTime, "[0-9]+"))
+paymentsPC_TOR$ResolveTime <- as.numeric(str_extract_all(paymentsPC_TOR$ResolveTime, "[0-9]+"))
+paymentsPC_TOR$TotalFeesMilisatoshis <- as.numeric(str_extract_all(paymentsPC_TOR$TotalFeesMilisatoshis, "[0-9]+"))
+paymentsPC_TOR$TransactionValue <- as.numeric(str_extract_all(paymentsPC_TOR$TransactionValue, "[0-9]+"))
+
+#save dataframe as .xlsx
+write.xlsx(paymentsPC_TOR, "paymentsPC_TOR_cleaned.xlsx")
